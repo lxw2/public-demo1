e3b61e77832444dd0df49a7b7fe9f944daadcffc
lxw2 <51943013+lxw2@users.noreply.github.com>
2019-08-29 19:28:41 +0800

    Update helloworld.java

--- projectDemo1/mode1/src/helloword/helloworld.java
+++ projectDemo1/mode1/src/helloword/helloworld.java
@@ -3,5 +3,6 @@ package helloword;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello world !");
+        System.out.println("我进行了修改");
     }
 }
